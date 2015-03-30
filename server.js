bbe7539b714d2fcd92764fdd50059724b2605f11
gchaville <gillian.chaville@gmail.com>
2015-03-30 16:25:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.get('/', function(req, res) {
-    res.sendfile('index.html');
-});
+app.use (express.static (__dirname + '/graph'));
 
 server.listen(8070, function(){
   console.log('listening on localhost:8070');
